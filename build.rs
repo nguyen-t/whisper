d80194f3f45ed1e7fa0fffa5c509513b3cac75d4
nguyen-t <5609219+nguyen-t@users.noreply.github.com>
2024-01-04 22:50:04 -0600

    Build logs

--- build.rs
+++ build.rs
@@ -26,6 +26,7 @@ fn compile() -> Result<(), Error> {
     .pic(true);
 
   #[cfg(target_os = "openbsd")] {
+    println!("OS: OpenBSD");
     cc_
       .define("_XOPEN_SOURCE", "700")
       .define("_BSD_SOURCE", None);
@@ -34,36 +35,42 @@ fn compile() -> Result<(), Error> {
       .define("_BSD_SOURCE", None);;
   }
   #[cfg(target_os = "linux")] {
+    println!("OS: Linux");
     cc_
       .define("_GNU_SOURCE", None);
     cxx_
       .define("_GNU_SOURCE", None);
   }
   #[cfg(target_os = "macos")] {
+    println!("OS: macOS");
     cc_
       .define("_DARWIN_C_SOURCE", None);
     cxx_
       .define("_DARWIN_C_SOURCE", None);
   }
   #[cfg(target_os = "dragonfly")] {
+    println!("OS: Dragonfly");
     cc_
       .define("__BSD_VISIBLE", None);
     cxx_
       .define("__BSD_VISIBLE", None);
   }
   #[cfg(target_os = "freebsd")] {
+    println!("OS: FreeBSD");
     cc_
       .define("__BSD_VISIBLE", None);
     cxx_
       .define("__BSD_VISIBLE", None);
   }
   #[cfg(target_os = "netbsd")] {
+    println!("OS: NetBSD");
     cc_
       .define("_NETBSD_SOURCE", None);
     cxx_
       .define("_NETBSD_SOURCE", None);
   }
   #[cfg(target_family = "unix")] {
+    println!("Family: Unix");
     cc_
       .flag("-pthread");
     cxx_
@@ -71,36 +78,42 @@ fn compile() -> Result<(), Error> {
   }
 
   #[cfg(target_feature = "avx")] {
+    println!("Feature: AVX");
     cc_
       .flag("-mavx");
     cxx_
       .flag("-mavx");
   }
   #[cfg(target_feature = "avx2")] {
+    println!("Feature: AVX2");
     cc_
       .flag("-mavx2");
     cxx_
       .flag("-mavx2");
   }
   #[cfg(target_feature = "fma")] {
+    println!("Feature: FMA");
     cc_
       .flag("-mfma");
     cxx_
       .flag("-mfma");
   }
   #[cfg(target_feature = "f16c")] {
+    println!("Feature: F16C");
     cc_
       .flag("-mf16c");
     cxx_
       .flag("-mf16c");
   }
   #[cfg(target_feature = "sse3")] {
+    println!("Feature: SSE3");
     cc_
       .flag("-msse3");
     cxx_
       .flag("-msse3");
   }
   #[cfg(target_feature = "ssse3")] {
+    println!("Feature: SSSE3");
     cc_
       .flag("-mssse3");
     cxx_
