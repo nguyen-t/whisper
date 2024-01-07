9d47ede1643e158db735f4d8e3dbd7aab3bcceca
nguyen-t <5609219+nguyen-t@users.noreply.github.com>
2024-01-07 14:58:16 -0600

    Added tests for x86_64-unknown-linux-gnu

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,5 +1,20 @@
-import test from 'ava'
+import test from 'ava';
+import { readFileSync } from 'node:fs';
+import { Whisper, WhisperSamplingStrategy } from '../index.js';
 
-test('Test', (t) => {
-  t.is(1, 1);
+test('Whisper initialization', (t) => {
+  const whisper = new Whisper('whisper/models/for-tests-ggml-tiny.en.bin')
+    .strategy(WhisperSamplingStrategy.GREEDY, 2)
+    .nThreads(1)
+    .nMaxTextCtx(-1)
+    .language("en")
+    .entropyThold(2.40)
+    .logprobThold(-1.00)
+    .temperatureInc(0.20);
+
+  t.notThrows(() => {
+    const file = readFileSync('whisper/samples/jfk.wav');
+
+    whisper.infer(file);
+  });
 });
